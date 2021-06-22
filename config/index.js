432f7cced37f67eb72d1c05654ae2515935911bf
hefengen <hefengen@hotmail.com>
2021-06-22 21:46:11 +0800

    init:初始化项目

---
+++ config/index.js
@@ -0,0 +1,8 @@
+const config = {
+}
+module.exports = function (merge) {
+    if (process.env.NODE_ENV === 'development') {
+        return merge({}, config, require('./dev'))
+    }
+    return merge({}, config, require('./prod'))
+}
